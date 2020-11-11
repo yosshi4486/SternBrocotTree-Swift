966f98f3dae7810b9bf85c96ed2e5b48a7be6a6a
yosshi4486 <yoshiharuseijin@gmail.com>
2020-11-12 08:45:43 +0900

    feat: Add gcd

--- Sources/SternBroctTreeSwift/MathHelpers.swift
+++ Sources/SternBroctTreeSwift/MathHelpers.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+/// Returns a greatest common divider(gcd) for the given factors.
+func gcd(_ a: Int32, _ b: Int32) -> Int32 {
+
+    var a = a
+    var b = b
+    var tmp: Int32
+
+    while b != 0 {
+        tmp = a % b
+        a = b
+        b = tmp
+    }
+
+    return a
+}
