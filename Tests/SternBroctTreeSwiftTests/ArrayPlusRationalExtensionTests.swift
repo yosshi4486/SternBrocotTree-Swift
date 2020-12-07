3cf09295a07fdfb111904e9c594888b880368dc1
yosshi4486 <yoshiharuseijin@gmail.com>
2020-12-07 20:48:06 +0900

    refactor: Remove unused implementations.

--- Tests/SternBroctTreeSwiftTests/ArrayPlusRationalExtensionTests.swift
+++ Tests/SternBroctTreeSwiftTests/ArrayPlusRationalExtensionTests.swift
@@ -10,63 +10,8 @@ import XCTest
 
 class ArrayPlusRationalExtensionTests: XCTestCase {
 
-    struct StubRationalOrderable : RationalOrderable {
-        var rational: Rational
-    }
-
-    func testAppendingNil() throws {
-
-        // Preparation
-        let array = Array<StubRationalOrderable>()
-
-        // Execution
-        let result = try array.rationalForAppending()
-
-        // Assertion
-        XCTAssertNil(result)
-    }
-
-    func testAppending() throws {
-
-        // Preparation
-        var array = Array<StubRationalOrderable>()
-        array.append(.init(rational: try Rational(fraction: "1/1")!))
-
-        // Execution
-        let result = try array.rationalForAppending()
-
-        // Assertion
-        XCTAssertEqual(result?.description, "2/1")
-    }
-
-    func testInsertingSingle() throws {
-
-        // Preparation
-        let array = Array<StubRationalOrderable>()
-
-        // Execution
-        let result = try array.rationalForInserting(between: .init(rational: try Rational(fraction: "3/2")!),
-                                                    and: .init(rational: try Rational(fraction: "2/1")!))
-
-        // Assertion
-        XCTAssertEqual(result.first?.description, "5/3")
-    }
-
-    func testInsertingCollection() throws {
-
-        // Preparation
-        let array = Array<StubRationalOrderable>()
-
-        // Execution
-        let result = try array.rationalForInserting(between: .init(rational: try Rational(fraction: "3/2")!),
-                                                    and: .init(rational: try Rational(fraction: "2/1")!),
-                                                    numberOfItems: 3)
-
-        // Assertion
-        XCTAssertEqual(result.count, 3)
-        XCTAssertEqual(result[0].description, "5/3")
-        XCTAssertEqual(result[1].description, "7/4")
-        XCTAssertEqual(result[2].description, "9/5")
+    struct StubRationalOrderable : RationalUserOrderable {
+        var userOrder: Rational
     }
 
     func testMakeFibonacciPathArray() {
@@ -78,16 +23,16 @@ class ArrayPlusRationalExtensionTests: XCTestCase {
         array = makeFibonacciPathArray()
 
         //Assertion
-        XCTAssertEqual(array[0].rational.description, "1/2")
-        XCTAssertEqual(array[1].rational.description, "3/5")
-        XCTAssertEqual(array[2].rational.description, "8/13")
-        XCTAssertEqual(array[3].rational.description, "5/8")
-        XCTAssertEqual(array[4].rational.description, "2/3")
-        XCTAssertEqual(array[5].rational.description, "1/1")
+        XCTAssertEqual(array[0].userOrder.description, "1/2")
+        XCTAssertEqual(array[1].userOrder.description, "3/5")
+        XCTAssertEqual(array[2].userOrder.description, "8/13")
+        XCTAssertEqual(array[3].userOrder.description, "5/8")
+        XCTAssertEqual(array[4].userOrder.description, "2/3")
+        XCTAssertEqual(array[5].userOrder.description, "1/1")
 
         // Visualize
         for element in array {
-            print("fraction: \(element.rational), float: \(element.rational.float64Value)")
+            print("fraction: \(element.userOrder), float: \(element.userOrder.float64Value)")
         }
     }
 
@@ -100,36 +45,36 @@ class ArrayPlusRationalExtensionTests: XCTestCase {
         array.normalize()
 
         // Assertion
-        XCTAssertEqual(array[0].rational.description, "1/1")
-        XCTAssertEqual(array[1].rational.description, "2/1")
-        XCTAssertEqual(array[2].rational.description, "3/1")
-        XCTAssertEqual(array[3].rational.description, "4/1")
-        XCTAssertEqual(array[4].rational.description, "5/1")
-        XCTAssertEqual(array[5].rational.description, "6/1")
+        XCTAssertEqual(array[0].userOrder.description, "1/1")
+        XCTAssertEqual(array[1].userOrder.description, "2/1")
+        XCTAssertEqual(array[2].userOrder.description, "3/1")
+        XCTAssertEqual(array[3].userOrder.description, "4/1")
+        XCTAssertEqual(array[4].userOrder.description, "5/1")
+        XCTAssertEqual(array[5].userOrder.description, "6/1")
     }
 
     // Average 0.008
     func testNormalizePerformance() throws {
         
         // Preparation
-        var array = (1..<100000).compactMap({ try? Rational(numerator: 1, denominator: $0)} ).map({ StubRationalOrderable(rational: $0) })
+        var array = (1..<100000).compactMap({ try? Rational(numerator: 1, denominator: $0)} ).map({ StubRationalOrderable(userOrder: $0) })
         
         let comparator = try Rational(fraction: "1/1")!
-        XCTAssertTrue(array.allSatisfy({ $0.rational <= comparator }))
+        XCTAssertTrue(array.allSatisfy({ $0.userOrder <= comparator }))
         
         // Execution and Measurement
         measure {
             array.normalize()
         }
         
-        XCTAssertTrue(array.allSatisfy({ $0.rational >= comparator }))
+        XCTAssertTrue(array.allSatisfy({ $0.userOrder >= comparator }))
     }
 
     func makeFibonacciPathArray() -> Array<StubRationalOrderable> {
         return (0...5)
             .compactMap({ try? Rational(numerator: fib(n: $0), denominator: fib(n: $0 + 1)) })
             .sorted()
-            .map({ StubRationalOrderable(rational: $0) })
+            .map({ StubRationalOrderable(userOrder: $0) })
     }
 
     func fib(n: Int32) -> Int32 {
