f9aab9a75fa6f2485a1cfaebbb17ea5886a866dd
yosshi4486 <yoshiharuseijin@gmail.com>
2021-01-31 18:19:42 +0900

    refactor: Matrix2x2 to SBMatrix2x2.

--- Sources/SternBrocotTreeSwift/Core/Metrix2x2.swift
+++ Sources/SternBrocotTreeSwift/Core/Metrix2x2.swift
@@ -14,19 +14,43 @@ import Foundation
 ///     a b
 ///     c d
 ///
-public struct Matrix2x2 {
+public struct SBMatrix2x2 : SBTreeNode {
+
+    public static var identity: SBMatrix2x2 {
+        return SBMatrix2x2(a: 1, b: 0, c: 0, d: 1)
+    }
+
+    /// The metrix for generating left matrix form node.
+    public static var L: SBMatrix2x2 {
+        return SBMatrix2x2(a: 1, b: 0, c: 1, d: 1)
+    }
+
+    /// The metrix for generating right matrix form node.
+    public static var R: SBMatrix2x2 {
+        return SBMatrix2x2(a: 1, b: 1, c: 0, d: 1)
+    }
 
     /// The value which is positioned at top left.
-    var a: Int
+    public var a: Int
 
     /// The value which is positioned at top right.
-    var b: Int
+    public var b: Int
 
     /// The value which is positioned at bottom left.
-    var c: Int
+    public var c: Int
 
     /// The value which is positioned at bottom right.
-    var d: Int
+    public var d: Int
+
+    /// Returns a rational represented instance of this value.
+    public var rationalRepresentation: Rational {
+        return Rational(numerator: a + b, denominator: c + d)
+    }
+
+    /// Returns a determinants of this matrix.
+    public var determinants: Int {
+        return (a * d) - (b * c)
+    }
 
     /// Multiplies two 2x2 matrix and produces thier product.
     ///
@@ -35,8 +59,8 @@ public struct Matrix2x2 {
     /// - Parameters:
     ///   - lhs: The first matrix to multipy.
     ///   - rhs: The second matrix to multiply.
-    public static func * (lhs: Matrix2x2, rhs: Matrix2x2) -> Matrix2x2 {
-        return Matrix2x2(
+    public static func * (lhs: SBMatrix2x2, rhs: SBMatrix2x2) -> SBMatrix2x2 {
+        return SBMatrix2x2(
             a: (lhs.a * rhs.a) + (lhs.b * rhs.c),
             b: (lhs.a * rhs.b) + (lhs.b * rhs.d),
             c: (lhs.c * rhs.a) + (lhs.d * rhs.c),
@@ -51,21 +75,40 @@ public struct Matrix2x2 {
     /// - Parameters:
     ///   - lhs: The first matrix to multipy.
     ///   - rhs: The second matrix to multiply.
-    public static func *= (lhs: inout Matrix2x2, rhs: Matrix2x2) {
+    public static func *= (lhs: inout SBMatrix2x2, rhs: SBMatrix2x2) {
         lhs = lhs * rhs
     }
 
+    /// Moves to left.
+    public mutating func moveLeft() {
+        self *= SBMatrix2x2.L
+    }
+
+    /// Moves to right.
+    public mutating func moveRight() {
+        self *= SBMatrix2x2.R
+    }
+
+    /// Returns a new 2x2 matrix which positioned at left of this matrix.
+    public func makeLeft() -> SBMatrix2x2 {
+        return self * SBMatrix2x2.L
+    }
+
+    /// Returns a new 2x2 matrix which positioned at right of this matrix.
+    public func makeRight() -> SBMatrix2x2 {
+        return self * SBMatrix2x2.R
+    }
 
 }
 
-extension Matrix2x2 : AdditiveArithmetic {
+extension SBMatrix2x2 : AdditiveArithmetic {
 
-    public static var zero: Matrix2x2 {
-        return Matrix2x2(a: 0, b: 0, c: 0, d: 0)
+    public static var zero: SBMatrix2x2 {
+        return SBMatrix2x2(a: 0, b: 0, c: 0, d: 0)
     }
 
-    public static func + (lhs: Matrix2x2, rhs: Matrix2x2) -> Matrix2x2 {
-        return Matrix2x2(
+    public static func + (lhs: SBMatrix2x2, rhs: SBMatrix2x2) -> SBMatrix2x2 {
+        return SBMatrix2x2(
             a: lhs.a + rhs.a,
             b: lhs.b + rhs.b,
             c: lhs.c + rhs.c,
@@ -73,8 +116,8 @@ extension Matrix2x2 : AdditiveArithmetic {
         )
     }
 
-    public static func - (lhs: Matrix2x2, rhs: Matrix2x2) -> Matrix2x2 {
-        return Matrix2x2(
+    public static func - (lhs: SBMatrix2x2, rhs: SBMatrix2x2) -> SBMatrix2x2 {
+        return SBMatrix2x2(
             a: lhs.a - rhs.a,
             b: lhs.b - rhs.b,
             c: lhs.c - rhs.c,
@@ -84,56 +127,9 @@ extension Matrix2x2 : AdditiveArithmetic {
 
 }
 
-extension Matrix2x2 : Equatable { }
+extension SBMatrix2x2 : Equatable { }
 
-extension Matrix2x2 : SBTreeNode {
-
-    public static var identity: Matrix2x2 {
-        return Matrix2x2(a: 1, b: 0, c: 0, d: 1)
-    }
-
-    public var rationalRepresentation: Rational {
-        return Rational(numerator: a + b, denominator: c + d)
-    }
-    
-    /// The metrix for generating left matrix form node.
-    public static var L: Matrix2x2 {
-        return Matrix2x2(a: 1, b: 0, c: 1, d: 1)
-    }
-
-    /// The metrix for generating right matrix form node.
-    public static var R: Matrix2x2 {
-        return Matrix2x2(a: 1, b: 1, c: 0, d: 1)
-    }
-
-    /// Moves to left.
-    public mutating func moveLeft() {
-        self *= Matrix2x2.L
-    }
-
-    /// Moves to right.
-    public mutating func moveRight() {
-        self *= Matrix2x2.R
-    }
-
-    /// Returns a new 2x2 matrix which positioned at left of this matrix.
-    public func makeLeft() -> Matrix2x2 {
-        return self * Matrix2x2.L
-    }
-
-    /// Returns a new 2x2 matrix which positioned at right of this matrix.
-    public func makeRight() -> Matrix2x2 {
-        return self * Matrix2x2.R
-    }
-
-    /// Returns a determinants of this matrix.
-    public var determinants: Int {
-        return (a * d) - (b * c)
-    }
-
-}
-
-extension Matrix2x2 : CustomStringConvertible {
+extension SBMatrix2x2 : CustomStringConvertible {
 
     public var description: String {
         """
